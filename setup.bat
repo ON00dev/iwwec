83b518c2bf440291f283bd7735ab33e8e6de7802
ON00dev <vicguioliveira2005@gmail.com>
2024-07-22 10:09:27 -0300

    fix setup.bat

--- setup.bat
+++ setup.bat
@@ -4,6 +4,15 @@ setlocal
 REM Nome do executável
 set EXE_NAME=iwwerc.exe
 
+REM Solicita ao usuário para inserir o caminho do executável de origem
+set /p SRC_EXE="Enter the full path of the executable (e.g., C:\path\to\dist\iwwerc.exe): "
+
+REM Verifica se o executável de origem existe
+if not exist "%SRC_EXE%" (
+    echo Source executable not found: %SRC_EXE%
+    goto :EOF
+)
+
 REM Caminho do destino
 set DEST_DIR=%ProgramFiles%\iwwerC
 set DEST_EXE=%DEST_DIR%\%EXE_NAME%
@@ -14,16 +23,22 @@ if not exist "%DEST_DIR%" (
 )
 
 REM Copia o executável para o diretório de destino
-copy "dist\%EXE_NAME%" "%DEST_EXE%"
+copy "%SRC_EXE%" "%DEST_EXE%"
 
 REM Adiciona o diretório ao PATH do sistema, se não estiver presente
 setlocal enabledelayedexpansion
 set "CURRENT_PATH=%PATH%"
+set "DIR_IN_PATH=false"
+
 for %%A in ("%PATH:;=" "%") do (
     if /I "%%~A"=="%DEST_DIR%" (
-        echo Directory already in PATH.
-        goto :EOF
+        set "DIR_IN_PATH=true"
     )
+)
+
+if "!DIR_IN_PATH!"=="true" (
+    echo Directory already in PATH.
+    goto :EOF
 )
 
 REM Adiciona o diretório ao PATH do sistema
